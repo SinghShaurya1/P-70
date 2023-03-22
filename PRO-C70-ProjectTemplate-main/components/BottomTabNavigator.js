eba6e52e48c19b6781e8fbd509b4b4441a930e2f
SinghShaurya1 <104992391+SinghShaurya1@users.noreply.github.com>
2023-03-22 19:12:02 +0530

    Update BottomTabNavigator.js

--- PRO-C70-ProjectTemplate-main/components/BottomTabNavigator.js
+++ PRO-C70-ProjectTemplate-main/components/BottomTabNavigator.js
@@ -26,11 +26,11 @@ export default class BottomTabNavigator extends Component {
               //   iconName = "time";
               // }
 
-              // if (route.name === "Ride") {
-              //   iconName = "bicycle";
-              // } else if (route.name === "Ride History") {
-              //   iconName = "time";
-              // }
+               if (route.name === "Ride") {
+                 iconName = "bicycle";
+               } else if (route.name === "Ride History") {
+                 iconName = "time";
+               }
 
               return (
                 <Ionicons
